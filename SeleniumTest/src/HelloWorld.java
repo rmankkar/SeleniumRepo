3a682e8f382898eaee218e749a9e896256fd6e82
rmankkar <rishimankkar91@gmail.com>
2020-03-30 22:21:57 +0530

    Added Driver initialization logic

--- SeleniumTest/src/HelloWorld.java
+++ SeleniumTest/src/HelloWorld.java
@@ -1,10 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.println("Hello Selenium with Git!!!");
+		
+		System.setProperty("webdriver.gecko.driver", "R:\\Selenium\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.google.com/");
+		
 		
 	}
 
